83d0eae4d67efa86ae06088ebb7e009097b5d7d2
cobee <18027041863@163.com>
2020-10-24 23:16:00 +0800

    项目初始化

---
+++ src/main/java/com/softd/test/springcloud/usercenter/SpringCloudUsercenterApplication.java
@@ -0,0 +1,13 @@
+package com.softd.test.springcloud.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudUsercenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUsercenterApplication.class, args);
+    }
+
+}

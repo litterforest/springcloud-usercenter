322f298d77a0b5080b8ebd63bd8873b074b3832c
cobee <18027041863@163.com>
2020-10-31 22:52:07 +0800

    简单配置安全功能

--- src/main/java/com/softd/test/springcloud/usercenter/service/UserServiceImpl.java
+++ src/main/java/com/softd/test/springcloud/usercenter/service/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
